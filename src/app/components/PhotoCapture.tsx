f321bf8c6b049c1fb6b1f88bef198df3b840aab2
theashu02 <ashutoshchauhan1919@gmail.com>
2025-07-14 23:50:24 +0530

    add some component in the app

---
+++ src/app/components/PhotoCapture.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function PhotoCapture() {
+  return (
+    <div>photo-capture</div>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { Button } from "@/components/ui/button";
+import MapView from "./components/MapView";
+import NotificationManager from "./components/NotificationManager";
+import PhotoCapture from "./components/PhotoCapture";
 
 export default function Home() {
   return (
     <div>
       {" "}
       <Button variant="destructive">Home</Button>
+      <MapView />
+      <NotificationManager />
+      <PhotoCapture />
     </div>
   );
 }
